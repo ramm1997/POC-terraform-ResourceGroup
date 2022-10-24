f18e66824e434e0cfd1009d9bd6acca5f9720129
ramm1997 <80684861+ramm1997@users.noreply.github.com>
2022-10-24 07:58:15 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,16 @@
+variable "SubcriptionsID" {
+    type = string
+}
+
+variable "TanentID" { 
+    type = string
+}
+
+variable "ClientID" {
+    type = string
+
+}
+
+variable "ClientSecret" { 
+    type = string
+}
