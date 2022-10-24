25218883514c9866256a27151d0094539d72c51d
ramm1997 <80684861+ramm1997@users.noreply.github.com>
2022-10-24 07:50:18 +0530

    Update ResourceGroup.tf

--- ResourceGroup.tf
+++ ResourceGroup.tf
@@ -1,7 +1,19 @@
+#Establish connection by service principal!
+
+
 provider "azurerm" {
   # Configuration options
-   features {}
+  
+  version = "3.28.0"
+  
+  SubcriptionsID = var.SubcriptionsID
+  ClientID = var.ClientID
+  ClientSecret = var.ClientSecret
+  TanentID = var.TanentID
+  
+  features {}
 }
+
 
 resource "azurerm_resource_group" "ResourceGroup-POC-RG" {
   name     = "poc-resources"
